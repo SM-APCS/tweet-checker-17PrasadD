ca7c856258b7dcd635caa8cdf45056d290d86473
17PrasadD <dhruv@10.1.33.89>
2016-10-28 13:41:52 -0500

    count twitter characters

--- TweetTester/src/tweettester/TweetTester.java
+++ TweetTester/src/tweettester/TweetTester.java
@@ -21,6 +21,12 @@ public class TweetTester {
         Scanner scan = new Scanner (System.in);
          String tweet;
          
+         tweet = scan.nextLine();
+                 
+         System.out.println(tweet.length());
+         
+         
+         
     }
     
 }
